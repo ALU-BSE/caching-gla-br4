3e284b2d7f7e7de85878a3a7b7008b3862840889
JOHN-KWIZERA <j.kwizera@alustudent.com>
2025-09-19 11:55:08 +0200

    helper

--- users/views.py
+++ users/views.py
@@ -10,6 +10,13 @@ from users.serializers import UserSerializer
 from django.conf import settings
 from rest_framework.response import Response
 
+
+def get_cache_key(prefix, identifier=None):
+    """Generate consistent cache keys"""
+    if identifier:
+        return f"{prefix}_{identifier}"
+    return prefix
+    
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
